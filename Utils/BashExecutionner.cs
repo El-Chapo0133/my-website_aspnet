ad303b9b27aa3e435b63160831793b6541cc6af5
El-Chapo0133 <levequeloris66@gmail.com>
2022-05-31 22:21:32 +0200

    Minor change for test

--- Utils/BashExecutionner.cs
+++ Utils/BashExecutionner.cs
@@ -1,15 +1,17 @@
 namespace my_website.Utils;
 
 static class BashExecutionner {
-        public static void ExecuteCommand(string command) {
+        public static int ExecuteCommand(string command) {
                 System.Diagnostics.ProcessStartInfo process = new System.Diagnostics.ProcessStartInfo();
                 process.UseShellExecute = false;
                 process.FileName = "/bin/bash";
                 process.Arguments = "-c \" " + command + " \"";
                 process.RedirectStandardOutput = true;
 
-                System.Diagnostics.Process cmd =  System.Diagnostics.Process.Start(process);
+                System.Diagnostics.Process? cmd =  System.Diagnostics.Process.Start(process);
+                if (cmd is null) return 1;
                 // waiting to complete 
                 cmd.WaitForExit();
+                return 0;
         }
 }
