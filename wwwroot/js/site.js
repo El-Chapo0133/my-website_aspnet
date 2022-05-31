1ff19313b3a5bd2d515974b94e928b04c52fb49f
El-Chapo0133 <levequeloris66@gmail.com>
2022-05-31 21:31:09 +0200

    Updated nav

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const checkbox = document.getElementById('burger-check');
+const leftNav = document.getElementById('left-nav');
+const navItems = document.getElementsByClassName('nav-item');
+
+checkbox.addEventListener('change', (event) => {
+        if (event.currentTarget.checked) {
+                leftNav.style.height = "100vh";
+                // Array.prototype.forEach.call(navItems, item => {
+                //         item.style.transform = "translate-x(-20px)";
+                // });
+        } else {
+                // Array.prototype.forEach.call(navItems, item => {
+                //         item.style.transform = "translate-x(20px)";
+                // });
+                leftNav.style.height = "4rem";
+        }
+});
